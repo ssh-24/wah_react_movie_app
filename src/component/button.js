91f626bf139f2150a7ae8c6944f326048c60506e
minji0123 <sso02027@naver.com>
2022-10-10 23:19:15 +0900

    공통버튼 제작중

---
+++ src/component/button.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+import './css/button.css';
+
+const button = () => {
+    return (
+        <>
+            <div className='fixedBtn_wrap topBtn'>
+                <a className="btn_fixedTicketing">예매하기</a>
+                <a className="btn_gotoTop"><img src="https://img.cgv.co.kr/R2014/images/common/btn/gotoTop.png" alt="최상단으로 이동"/></a>
+            </div>
+        </>
+    )
+}
+
+export default button;
